cfc8a914d2d72ee089784a3945fea54f75bb606b
pauloofmeta <pauloofmeta@gmail.com>
2018-11-18 00:51:14 -0200

    Commit inicial

---
+++ CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,34 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task SalveCategoria(string nome);
+        Categoria GetCategoria(string nome);
+    }
+
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public Categoria GetCategoria(string nome)
+        {
+            return dbSet
+                .Where(c => c.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase))
+                .SingleOrDefault();
+        }
+
+        public async Task SalveCategoria(string nome)
+        {
+            if (!dbSet.Where(c => c.Nome.Equals(nome)).Any())
+                dbSet.Add(new Categoria(nome));
+            await contexto.SaveChangesAsync();
+        }
+    }
+}
